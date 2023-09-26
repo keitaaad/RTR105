592270cabc6d909315a9c07e2591c40076109d20
keitaaad <144106379+keitaaad@users.noreply.github.com>
2023-09-26 10:48:38 +0300

    Add files via upload

---
+++ Class_05/second.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+//vienas rindas komentārs
+/* komentāra bloka sākums
+...
+komentāra bloka beigas */
+int main()
+ {
+
+ printf("Hello World\n"); // \n - darbības simbols - kursoora pārcelšanana
+                         // jaunajā rindā
+                        // analogija ar echo -e "Hello World\n"
+                       // \a - darbības simbols - skaņa
+ printf("\a");
+ return 0;
+ }
